e1b3321ca1fbc686b4ee4f476d56707f2dd4c269
Dean Fox <deanfoxcd@gmail.com>
2025-01-14 16:58:36 -0500

    add basic layout style

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import '@/app/_styles/globals.css';
 import { Josefin_Sans, Inter } from 'next/font/google';
 import Logo from '@/app/_components/Logo';
 import Navigation from '@/app/_components/Navigation';
+import Header from './_components/Header';
 
 const josefin = Josefin_Sans({
   subsets: ['latin'],
@@ -26,14 +27,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+        className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
       >
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+        <Header />
+        <div className='flex-1 px-8 py-12'>
+          <main className='max-w-7xl mx-auto'>{children}</main>
+        </div>
       </body>
     </html>
   );
