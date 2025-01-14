a69554874c127bda40ae6b0b1e38a981622e03c7
Dean Fox <deanfoxcd@gmail.com>
2025-01-14 16:50:58 -0500

    add font

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,17 @@
 import '@/app/_styles/globals.css';
+import { Josefin_Sans, Inter } from 'next/font/google';
 import Logo from '@/app/_components/Logo';
 import Navigation from '@/app/_components/Navigation';
+
+const josefin = Josefin_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export const metadata = {
   title: {
@@ -14,7 +25,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className='bg-primary-950 text-primary-100 min-h-screen'>
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+      >
         <header>
           <Logo />
         </header>
