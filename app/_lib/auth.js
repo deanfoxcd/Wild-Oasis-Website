ffdc44f5f03a0ce7e09db1cef17df5953ac32974
Dean Fox <deanfoxcd@gmail.com>
2025-01-19 13:11:18 -0500

    add middleware to prevent access to guest area when not logged in

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,7 +1,14 @@
 import NextAuth from 'next-auth';
 import Google from 'next-auth/providers/google';
 
-const authConfig = { providers: [Google] };
+const authConfig = {
+  providers: [Google],
+  callbacks: {
+    authorized({ auth, request }) {
+      return !!auth?.user;
+    },
+  },
+};
 
 export const {
   handlers: { GET, POST },
